c22815799fb225159543ce6d1e573c15c83d8766
Gustavo Xavier <guh.coostta@gmail.com>
2020-09-28 22:20:13 -0300

    Métodos Estáticos

---
+++ classes/Utils.js
@@ -0,0 +1,13 @@
+class Utils {
+
+    static dateFormat(date) {
+
+        return  date.getDate() +
+                '/' + (date.getMonth() + 1) +
+                '/' + date.getFullYear() +
+                ' ' + date.getHours() + 
+                ':' + date.getMinutes()
+
+    }
+
+}
