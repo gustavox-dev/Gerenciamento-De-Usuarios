62c7f0f58bf593f7ba96d201a1aa78bc7969345f
Gustavo Xavier <guh.coostta@gmail.com>
2020-09-30 11:12:03 -0300

    Atualizando Estatísticas - Usando DataSet

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -20,6 +20,10 @@ class UserController {
 
             let values = this.getValues()
 
+            if(!values) return false
+
+
+
             this.getPhoto().then(
 
                 content => {
@@ -138,6 +142,8 @@ class UserController {
             
             let tr = document.createElement('tr')
 
+            tr.dataset.user = JSON.stringify(dataUser)
+
             tr.innerHTML = `
             <tr>
                 <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -154,5 +160,29 @@ class UserController {
 
 
             this.tableEl.appendChild(tr)
+
+            this.upDateCount()
+
+            
+        }
+
+        upDateCount(){
+
+            let numberUsers = 0;
+            let numberAdmin = 0;
+
+            [...this.tableEl.children].forEach(tr => {
+
+                numberUsers++
+
+
+                let user = JSON.parse(tr.dataset.user)
+
+                if (user._admin) numberAdmin++;
+            })
+
+            document.querySelector("#number-users").innerHTML = numberUsers
+            document.querySelector("#number-users-admin").innerHTML = numberAdmin
+
         }
     }
