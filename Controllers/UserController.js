ddb07b2b300ae1de9804f2fd7c5745bf774e40df
Gustavo Xavier <guh.coostta@gmail.com>
2020-10-02 16:21:47 -0300

    Excluindo dados do Objeto

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -226,7 +226,7 @@ class UserController {
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
             `;
@@ -241,6 +241,17 @@ class UserController {
         }
 
         addEventsTR(tr) {
+
+            tr.querySelector(".btn-delete").addEventListener("click", e => {
+
+                if (confirm("Deseja realmente excluir? ")) {
+
+                    tr.remove()
+
+                    this.updateCount()
+                }
+
+            })
 
             tr.querySelector(".btn-edit").addEventListener("click", e => {
                 
